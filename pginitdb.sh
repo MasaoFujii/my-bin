ae1863b1927a21a0330fd2eca91251018ea2928f
MasaoFujii <masao.fujii@gmail.com>
2009-11-25 19:42:54 +0900

    Add new script 'pginitdb.sh' to create an initial database cluster.

---
+++ pginitdb.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+CURDIR=$(pwd)
+PROGNAME=$(basename ${0})
+
+usage ()
+{
+    echo "${PROGNAME} creates an initial \$PGDATA"
+    echo ""
+    echo "Usage:"
+    echo "  ${PROGNAME} [OPTIONS]"
+    echo ""
+    echo "Options:"
+    echo "  -D        location of \$PGDATA"
+}
+
+PGBIN=${CURDIR}/bin
+PGDATA=${CURDIR}/data
+
+if [ ! -f ${PGBIN}/pg_config ]; then
+    echo "ERROR: invalid present location"
+    echo "HINT : you need to move to pgsql installation directory"
+    exit 1
+fi
+
+while getopts "D:h" OPT; do
+    case ${OPT} in
+	D)
+	    PGDATA=${OPTARG}
+	    ;;
+	h)
+	    usage
+	    exit 0
+	    ;;
+    esac
+done
+
+if [ ! -d ${PGDATA} ]; then
+    echo "ERROR: \$PGDATA is not found: ${PGDATA}"
+    exit 1
+fi
+
+rm -rf ${PGDATA}
+${PGBIN}/initdb -D $PGDATA --no-locale --encoding=UTF8
+
+echo "host all all 0.0.0.0/0 trust" >> ${PGDATA}/pg_hba.conf
+echo "listen_addresses = '*'"       >> ${PGDATA}/postgresql.conf
+echo "checkpoint_segments = 64"     >> ${PGDATA}/postgresql.conf
