7f641b3ff7f2349ae9230f0d4270cfa9aad9131c
MasaoFujii <masao.fujii@gmail.com>
2009-11-26 12:40:01 +0900

    Add new script "pghba.conf" which opens pg_hba.conf with emacs.

---
+++ pghba.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+CURDIR=$(pwd)
+PROGNAME=$(basename ${0})
+
+usage ()
+{
+    echo "${PROGNAME} opens pg_hba.conf with emacs"
+    echo ""
+    echo "Usage:"
+    echo "  ${PROGNAME} [PGDATA]"
+    echo ""
+    echo "Options:"
+    echo "  -h        shows this help, then exits"
+}
+
+PGBIN=${CURDIR}/bin
+PGDATA=${CURDIR}/data
+
+if [ ! -f ${PGBIN}/pg_config ]; then
+    echo "ERROR: invalid present location"
+    echo "HINT : you need to move to pgsql installation directory"
+    exit 1
+fi
+
+while getopts "h" OPT; do
+    case ${OPT} in
+	h)
+	    usage
+	    exit 0
+	    ;;
+    esac
+done
+
+shift $(expr ${OPTIND} - 1)
+
+if [ ${#} -gt 0 ]; then
+    PGDATA=${1}
+fi
+
+if [ ! -d ${PGDATA} ]; then
+    echo "ERROR: \$PGDATA is not found: ${PGDATA}"
+    exit 1
+fi
+
+emacs ${PGDATA}/pg_hba.conf &
