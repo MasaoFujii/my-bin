2be8385c0c52b2e81ec6a828ca87e2dfd1ff887d
MasaoFujii <masao.fujii@gmail.com>
2009-12-03 21:54:49 +0900

    Fix the bug that pginitdb.sh adds listen_addresses parameter into conf file of pgsql 7.4 wrongly.
    listen_addresses can not be handled by pgsql 7.4. Instead tcpip_socket
    should be set to true.

--- pginitdb.sh
+++ pginitdb.sh
@@ -1,34 +1,31 @@
 #!/bin/sh
 
-# Load common definitions
 . pgcommon.sh
 
-# Show usage
 Usage ()
 {
 	UsageForHelpOption "creates an initial database cluster"
 }
 
-# Set up minimal settings
 SetupMinimalSettings ()
 {
-	SetOneGuc listen_addresses "'*'" ${PGCONF}
+	if [ $PGMAJOR -le 74 ]; then
+		SetOneGuc tcpip_socket true ${PGCONF}
+	else
+		SetOneGuc listen_addresses "'*'" ${PGCONF}
+	fi
 	SetOneGuc checkpoint_segments 64 ${PGCONF}
 	echo "host	all	all	0.0.0.0/0	trust" >> ${PGHBA}
 }
 
-# Check that we are in the pgsql installation directory
 CurDirIsPgsqlIns
 
-# Parse command-line arguments
 ParsingForHelpOption ${@}
 GetPgData ${@}
 
-# Delete old $PGDATA if pgsql is NOT running
 PgsqlMustNotRunning
 rm -rf ${PGDATA}
 
-# Create initial database cluster
 PGLOCALE=C
 PGENCODING=UTF8
 ${PGBIN}/initdb -D ${PGDATA} --locale=${PGLOCALE} --encoding=${PGENCODING}
