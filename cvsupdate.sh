648aba0d8f65801c83e10ff27bfcb579bf972331
MasaoFujii <masao.fujii@gmail.com>
2009-12-03 15:14:15 +0900

    Add new script 'cvsupdate.sh' which updates downloads cvs head source and compiles it.

---
+++ cvsupdate.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+. pgcommon.sh
+
+PREFIX=/dav/head-pgsql
+
+usage ()
+{
+	echo "$PROGNAME downloads and compiles the CVS HEAD"
+	echo ""
+	echo "Usage:"
+	echo "  $PROGNAME [PREFIX]"
+	echo ""
+	echo "The default PREFIX is \"$PREFIX\"."
+}
+
+CurDirIsPgsqlSrc
+
+if [ ! -d $CURDIR/CVS ]; then
+	echo "$PROGNAME: here \"$CURDIR\" is not CVS directory"
+	exit 1
+fi
+
+while [ $# -gt 0 ]; do
+	case "$1" in
+		-h|--help|"-\?")
+			usage
+			exit 0;;
+		*)
+			echo "$PROGNAME: invalid option: $1" 1>&2
+			exit 1;;
+	esac
+	shift
+done
+
+pgclean.sh -a
+cvs update
+pgetags.sh
+rm -rf $PREFIX
+pgmake.sh $PREFIX debug
