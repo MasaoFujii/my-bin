b81fea54b81ecd26c8af1c3d8bb332d002696ae6
MasaoFujii <masao.fujii@gmail.com>
2009-12-04 20:20:15 +0900

    Add new script 'pgrm.sh' which removes all unused database cluster, archive and backup directories from current directory.

---
+++ pgrm.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+. pgcommon.sh
+
+usage ()
+{
+	echo "$PROGNAME removes pgsql directories"
+	echo ""
+	echo "Usage:"
+	echo "  $PROGNAME"
+	echo ""
+	echo "Description:"
+	echo "  All unused database cluster, archive and backup directories"
+	echo "  are removed from current directory."
+}
+
+CurDirIsPgsqlIns
+
+while [ $# -gt 0 ]; do
+	case "$1" in
+		-h|--help|"-\?")
+			usage
+			exit 0;;
+		*)
+			echo "$PROGNAME: invalid option: $1" 1>&2
+			exit 1;;
+	esac
+	shift
+done
+
+pgdata_children="base global pg_clog pg_xlog"
+
+removal_pgdata ()
+{
+	pgdata="$1"
+
+	if [ ! -d "$pgdata" ]; then
+		return 1
+	fi
+
+	for child in $pgdata_children; do
+		if [ ! -d "$pgdata/$child" ]; then
+			return 1
+		fi
+	done
+
+	$PGBIN/pg_ctl -D "$pgdata" status > /dev/null 2>&1
+	if [ $? -eq 0 ]; then
+		return 1
+	fi
+
+	return 0
+}
+
+for pgdata in $(ls $CURDIR); do
+	removal_pgdata "$pgdata"
+	if [ $? -eq 0 ]; then
+		echo "rm -rf $pgdata"
+		rm -rf "$pgdata"
+		echo "rm -rf $pgdata.arch"
+		rm -rf "$pgdata.arch"
+		echo "rm -rf $pgdata.bkp"
+		rm -rf "$pgdata.bkp"
+	fi
+done
