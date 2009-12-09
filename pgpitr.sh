b3c144957120152c13fe99ca9e4d2a9250f4b6c4
MasaoFujii <masao.fujii@gmail.com>
2009-12-09 17:34:25 +0900

    Change pgpitr.sh so that it uses the latest pg_xlog.

--- pgpitr.sh
+++ pgpitr.sh
@@ -1,40 +1,65 @@
 #!/bin/sh
 
-# Load common definitions
 . pgcommon.sh
 
-# Show usage
-Usage ()
+usage ()
 {
-	UsageForHelpOption "prepares for an archive recovery"
+	echo "$PROGNAME prepares for an archive recovery"
+	echo ""
+	echo "Usage:"
+	echo "  $PROGNAME [PGDATA]"
+	echo ""
+	echo "Description:"
+	echo "  This utility restores a base backup and creates recovery.conf."
+	echo "  This must be called after a base backup has been taken."
 }
 
-# Prepare for an archive recovery
-PrepareForArchiveRecovery ()
-{
-	# Replace the old $PGDATA with a base backup
-	rm -rf ${PGDATA}
-	cp -r ${PGDATABKP} ${PGDATA}
+while [ $# -gt 0 ]; do
+	case "$1" in
+		-h|--help|"-\?")
+			usage
+			exit 0;;
+		-*)
+			echo "$PROGNAME: invalid option: $1" 1>&2
+			exit 1;;
+		*)
+			PGDATA="$1";;
+	esac
+	shift
+done
 
-	# Replace the old archive directory with a backup if it exists
-	if [ -d ${PGARCHBKP} ]; then
-		rm -rf ${PGARCH}
-		cp -r ${PGARCHBKP} ${PGARCH}
-	fi
-
-	# Create recovery.conf and specify restore_command
-	RESTORECMD="cp ../${PGARCHNAME}/%f %p"
-    echo "restore_command = '${RESTORECMD}'" > ${RECOVERYCONF}
-}
-
-# Check that we are in the pgsql installation directory
 CurDirIsPgsqlIns
 
-# Parse command-line arguments
-ParsingForHelpOption ${@}
-GetPgData ${@}
-ValidatePgData
+if [ ! -d $PGDATA ]; then
+	echo "$PROGNAME: database cluster is not found: \"$PGDATA\"" 1>&2
+	exit 1
+fi
 
-# Prepare for an archive recovery if pgsql is NOT running
 PgsqlMustNotRunning
-PrepareForArchiveRecovery
+
+PGARCH=$PGDATA.arch
+PGARCHNAME=$(basename $PGARCH)
+PGDATABKP=$PGDATA.bkp
+PGARCHBKP=$PGARCH.bkp
+
+if [ ! -d $PGDATABKP ]; then
+	echo "$PROGNAME: base backup is not found: \"$PGDATABKP\"" 1>&2
+	exit 1
+fi
+
+if [ ! -d $PGARCH ]; then
+	echo "$PROGNAME: archive directory is not found: \"$PGARCH\"" 1>&2
+	exit 1
+fi
+
+RESTORECMD="cp ../${PGARCHNAME}/%f %p"
+echo "restore_command = '${RESTORECMD}'" > $PGDATABKP/recovery.conf
+
+rm -rf $PGDATABKP/pg_xlog
+mv $PGDATA/pg_xlog $PGDATABKP
+
+rm -rf $PGDATA
+cp -r $PGDATABKP $PGDATA
+
+rm -rf $PGARCHBKP
+cp -r $PGARCH $PGARCHBKP
