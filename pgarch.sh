03bb2bed731e574d44c3ddbc40d929bd977214af
MasaoFujii <masao.fujii@gmail.com>
2010-04-30 14:51:20 +0900

    Adjust pgarch.sh for v9.0

--- pgarch.sh
+++ pgarch.sh
@@ -1,64 +1,46 @@
 #!/bin/sh
 
-# Load common definitions
 . pgcommon.sh
 
-# Local variables
-PGARCH_SUPPLY=
-
-# Show usage
-Usage ()
+usage ()
 {
-    echo "${PROGNAME} enables WAL archiving"
-    echo ""
-    echo "Usage:"
-    echo "  ${PROGNAME} [OPTIONS] [PGDATA]"
-    echo ""
-    echo "Options:"
-    echo "  -A PATH   specifies archive directory path"
-    echo "  -h        shows this help, then exits"
+	echo "$PROGNAME enables WAL archiving"
+	echo ""
+	echo "Usage:"
+	echo "  $PROGNAME [PGDATA]"
+	echo ""
+	echo "Description:"
+	echo "  This utility sets up the configuration parameters related to WAL archiving"
+	echo "  and creates the archival directory."
 }
 
-# Enable WAL archiving
-EnableWALArchiving ()
-{
-    if [ ${PGMAJOR} -ge 83 ]; then
-			set_guc archive_mode on ${PGCONF}
-    fi
-
-    set_guc archive_command "'cp %p ../${PGARCHNAME}/%f'" ${PGCONF}
-}
+while [ $# -gt 0 ]; do
+	case "$1" in
+		-h|--help|"-\?")
+			usage
+			exit 0;;
+		-*)
+			echo "$PROGNAME: invalid option: $1" 1>&2
+			exit 1;;
+		*)
+			update_pgdata "$1";;
+	esac
+	shift
+done
 
 here_is_installation
-
-# Parse options
-while getopts "A:h" OPT; do
-    case ${OPT} in
-	A)
-	    PGARCH_SUPPLY=${OPTARG}
-	    ;;
-	h)
-	    Usage
-	    exit 0
-	    ;;
-    esac
-done
-shift $(expr ${OPTIND} - 1)
-
-# Get and validate $PGDATA
-GetPgData ${@}
-ValidatePgData
-
-# WAL archiving must be supported in this pgsql version
 archiving_is_supported
+check_directory_exists $PGDATA "database cluster"
 
-# Create new archive directory
-if [ ! -z "${PGARCH_SUPPLY}" ]; then
-    PGARCH=${PGARCH_SUPPLY}
-    PGARCHNAME=$(basename ${PGARCH})
+rm -rf $PGARCH
+mkdir $PGARCH
+
+if [ $PGMAJOR -ge 90 ]; then
+	set_guc wal_level archive $PGCONF
 fi
-rm -rf ${PGARCH}
-mkdir ${PGARCH}
 
-# Enable WAL archiving
-EnableWALArchiving
+if [ $PGMAJOR -ge 83 ]; then
+	set_guc archive_mode on $PGCONF
+fi
+
+set_guc archive_command "'cp %p ../$(basename $PGARCH)/%f'" $PGCONF
