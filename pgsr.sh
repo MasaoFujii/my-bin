44dcd16cba3cae8a09395881354a7c2375c575ce
MasaoFujii <masao.fujii@gmail.com>
2011-07-06 18:59:33 +0900

    Change pgsr.sh so that it can set up multiple standby servers.

--- pgsr.sh
+++ pgsr.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 . pgcommon.sh
 
@@ -13,10 +13,12 @@ SBYDATA=$CURDIR/sby
 SBYCONF=$SBYDATA/postgresql.conf
 SBYPORT=5433
 SBYPREFIX=sby
+SBYNUM=1
+SBYMAX=16
 
 PGBKP=$ACTDATA.bkp
 TRIGGER=$CURDIR/trigger
-RECOVERYCONF=$SBYDATA/recovery.conf
+RECCONF=$SBYDATA/recovery.conf
 
 usage ()
 {
@@ -32,6 +34,7 @@ usage ()
 	echo "Options:"
 	echo "  -a, --archive    uses the archive"
 	echo "  -c, --conflict   creates standby query conflict"
+	echo "  -n  NUMBER       specifys number of standbys"
 	echo "  -p, --primary    sets up only primary server"
 	echo "  -q, --quit       shuts down servers with fast mode"
 	echo "  -s, --standby    sets up only standby server"
@@ -53,6 +56,9 @@ while [ $# -gt 0 ]; do
 		-h|--help|"-\?")
 			usage
 			exit 0;;
+		-n)
+			SBYNUM=$2
+			shift;;
 		-p|--primary)
 			ONLYACT="TRUE";;
 		-q|--quit)
@@ -85,7 +91,7 @@ setup_primary ()
 
 	set_guc port $ACTPORT $ACTCONF
 	set_guc log_line_prefix "'$ACTPREFIX '" $ACTCONF
-	set_guc max_wal_senders 5 $ACTCONF
+	set_guc max_wal_senders $(expr $SBYNUM + 4) $ACTCONF
 	set_guc wal_level hot_standby $ACTCONF
 	set_guc wal_keep_segments 32 $ACTCONF
 
@@ -99,39 +105,75 @@ setup_primary ()
 	pgstart.sh -w $ACTDATA
 }
 
+prepare_standbys ()
+{
+	NEXTID=0
+	for i in $(seq 1 $SBYNUM); do
+		for j in $(seq $NEXTID $SBYMAX); do
+			$PGBIN/pg_ctl -D $SBYDATA$j status > /dev/null
+			if [ $? -ne 0 ]; then
+				SBYID[$i]=$j
+				SBYDATA[$i]=$SBYDATA$j
+				SBYCONF[$i]=$SBYDATA$j/postgresql.conf
+				SBYPORT[$i]=$(expr $SBYPORT + $j)
+				SBYPREFIX[$i]=$SBYPREFIX$j
+				TRIGGER[$i]=$TRIGGER$j
+				RECCONF[$i]=$SBYDATA$j/recovery.conf
+
+				NEXTID=$(expr $j + 1)
+				if [ $NEXTID -gt $SBYMAX ]; then
+					elog "could not set up the specified number of standbys"
+				fi
+				break
+			fi
+		done
+	done
+}
+
 setup_standby ()
 {
-	pgsql_is_dead $SBYDATA
+	prepare_standbys
 
-	rm -rf $TRIGGER $SBYDATA
+	for i in $(seq 1 $SBYNUM); do
+		pgsql_is_dead ${SBYDATA[$i]}
+		rm -rf ${TRIGGER[$i]} ${SBYDATA[$i]}
+	done
 
 	pgbackup.sh $ACTDATA
-	cp -r $PGBKP $SBYDATA
 
-	set_guc port $SBYPORT $SBYCONF
-	set_guc log_line_prefix "'$SBYPREFIX '" $SBYCONF
-	set_guc hot_standby on $SBYCONF
+	for i in $(seq 1 $SBYNUM); do
+		cp -r $PGBKP ${SBYDATA[$i]}
 
-	echo "standby_mode = 'on'" >> $RECOVERYCONF
-	echo "primary_conninfo = 'host=localhost port=$ACTPORT'" >> $RECOVERYCONF
-	echo "trigger_file = '$TRIGGER'" >> $RECOVERYCONF
+		set_guc port ${SBYPORT[$i]} ${SBYCONF[$i]}
+		set_guc log_line_prefix "'${SBYPREFIX[$i]} '" ${SBYCONF[$i]}
+		set_guc hot_standby on ${SBYCONF[$i]}
 
-	if [ "$USEARCH" = "TRUE" ]; then
-		echo "restore_command = 'cp $ACTARCH/%f %p'" >> $RECOVERYCONF
-	fi
+		echo "standby_mode = 'on'" >> ${RECCONF[$i]}
+		echo "primary_conninfo = 'host=localhost port=$ACTPORT application_name=${SBYPREFIX[$i]}'" >> ${RECCONF[$i]}
+		echo "trigger_file = '${TRIGGER[$i]}'" >> ${RECCONF[$i]}
 
-	pgstart.sh $SBYDATA
+		if [ "$USEARCH" = "TRUE" ]; then
+			echo "restore_command = 'cp $ACTARCH/%f %p'" >> ${RECCONF[$i]}
+		fi
+
+		pgstart.sh ${SBYDATA[$i]}
+	done
 }
 
 if [ "$QUITMODE" = "TRUE" ]; then
 	pgshutdown.sh -f $ACTDATA
-	pgshutdown.sh -f $SBYDATA
+	for i in $(seq 0 $SBYMAX); do
+		$PGBIN/pg_ctl -D $SBYDATA$i status > /dev/null
+		if [ $? -eq 0 ]; then
+			pgshutdown.sh -f $SBYDATA$i
+		fi
+	done
 	exit 0
 fi
 
 if [ "$MKCONFLICT" = "TRUE" ]; then
 	pgsql_is_alive $ACTDATA
-	pgsql_is_alive $SBYDATA
+	pgsql_is_alive ${SBYDATA[1]}
 
 	TMPTBL=tmptable_$(date +%Y%m%d%H%M%S)
 
@@ -139,7 +181,7 @@ if [ "$MKCONFLICT" = "TRUE" ]; then
 	$PGBIN/psql -p $ACTPORT -c "INSERT INTO  $TMPTBL VALUES (1)"
 	sleep 1
 
-	$PGBIN/psql -p $SBYPORT -c "SELECT pg_sleep(60) FROM $TMPTBL" &
+	$PGBIN/psql -p ${SBYPORT[1]} -c "SELECT pg_sleep(60) FROM $TMPTBL" &
 	PSQLPID=$!
 	sleep 1
 
