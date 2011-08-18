9d3ee34bcc7e539b18202827ccd117ccd9eea99e
MasaoFujii <masao.fujii@gmail.com>
2011-08-18 22:09:50 +0900

    Change the default shutdown mode from "smart" to "fast".
    Also get rid of the obsolete option "-h".

--- pgshutdown.sh
+++ pgshutdown.sh
@@ -2,34 +2,32 @@
 
 . pgcommon.sh
 
+MODE="f"
+
 usage ()
 {
-	echo "$PROGNAME shuts down the postgres server"
+	echo "$PROGNAME shuts down PostgreSQL server."
 	echo ""
 	echo "Usage:"
 	echo "  $PROGNAME [OPTIONS] [PGDATA]"
 	echo ""
-	echo "Default:"
-	echo "  This utility performs a smart shutdown"
-	echo ""
 	echo "Options:"
-	echo "  -f, --fast        performs a fast shutdown"
-	echo "  -i, --immediate   performs an immediate shutdown"
-	echo "  -s, --smart       performs a smart shutdown"
+	echo "  -f    fast shutdown (default)"
+	echo "  -i    immediate shutdown"
+	echo "  -s    smart shutdown"
 }
 
-SHUTDOWN_MODE="s"
 while [ $# -gt 0 ]; do
 	case "$1" in
-		-f|--fast)
-			SHUTDOWN_MODE="f";;
-		-h|--help|"-\?")
+		"-?"|--help)
 			usage
 			exit 0;;
-		-i|--immediate)
-			SHUTDOWN_MODE="i";;
-		-s|--smart)
-			SHUTDOWN_MODE="s";;
+		-f)
+			MODE="f";;
+		-i)
+			MODE="i";;
+		-s)
+			MODE="s";;
 		-*)
 			elog "invalid option: $1";;
 		*)
@@ -41,4 +39,4 @@ done
 here_is_installation
 pgsql_is_alive
 
-$PGBIN/pg_ctl -D $PGDATA -m$SHUTDOWN_MODE stop
+$PGBIN/pg_ctl -D $PGDATA -m$MODE stop
