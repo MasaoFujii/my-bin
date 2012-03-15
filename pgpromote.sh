c17d04c4e7372c630f7e32d006f7cc9cb4353871
MasaoFujii <masao.fujii@gmail.com>
2012-03-16 04:43:02 +0900

    Add pgpromote.sh which promotes the server.

---
+++ pgpromote.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+. pgcommon.sh
+
+usage ()
+{
+	echo "$PROGNAME promotes PostgreSQL server."
+	echo ""
+	echo "Usage:"
+	echo "  $PROGNAME [PGDATA]"
+}
+
+while [ $# -gt 0 ]; do
+	case "$1" in
+		"-?"|--help)
+			usage
+			exit 0;;
+		-w)
+			OPT="-w";;
+		-*)
+			elog "invalid option: $1";;
+		*)
+			update_pgdata "$1";;
+	esac
+	shift
+done
+
+here_is_installation
+pgdata_exists
+pgsql_is_alive
+
+if [ $PGMAJOR -lt 82 ]; then
+	elog "warm-standby is not supported"
+fi
+
+if [ $PGMAJOR -ge 91 ]; then
+	$PGBIN/pg_ctl -D $PGDATA promote
+else
+	touch trigger
+fi
