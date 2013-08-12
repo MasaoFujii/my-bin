87fc7a1fdd6866eb6e166cc5c286f827250d2bdb
MasaoFujii <masao.fujii@gmail.com>
2013-08-12 19:38:44 +0900

    Add random_uppercase().

--- pgfuncs.sql
+++ pgfuncs.sql
@@ -15,3 +15,20 @@ BEGIN
 		RETURN result;
 END
 $$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION random_uppercase (num int) RETURNS text AS $$
+DECLARE
+    result text := '';
+BEGIN
+    LOOP
+        IF num = 0 THEN
+				    EXIT;
+			  ELSE
+				    num = num - 1;
+        END IF;
+        result := result || chr(floor(random() * 26)::int + 65);
+    END LOOP;
+
+		RETURN result;
+END
+$$ LANGUAGE plpgsql;
