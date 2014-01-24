5ae772f26b512c8b39a1e684378f7535a86e7539
MasaoFujii <masao.fujii@gmail.com>
2014-01-24 12:20:22 +0900

    Add new shell script "pggit.sh".
    This script does git operations in local
    PostgreSQL repository.

---
+++ pggit.sh
@@ -0,0 +1,95 @@
+#!/bin/sh
+
+. pgcommon.sh
+
+GITCMD=
+SUPPORTED_VERS="9_3 9_2 9_1 9_0 8_4"
+
+usage ()
+{
+cat <<EOF
+$PROGNAME does git operations in PostgreSQL local git repository.
+
+Usage:
+  $PROGNAME [COMMAND]
+
+Command:
+  help            shows help message (default)
+  merge-master    updates master and merges it into current branch
+  reset           resets current branch to HEAD
+  u[pdate]        updates master
+  update-all      updates master and all supported versions
+EOF
+}
+
+while [ $# -gt 0 ]; do
+	case "$1" in
+		"-?"|--help)
+			usage
+			exit 0;;
+		-*)
+			elog "invalid option: $1";;
+		*)
+			if [ -z "$GITCMD" ]; then
+				GITCMD="$1"
+			else
+				elog "too many arguments"
+			fi
+			;;
+	esac
+	shift
+done
+
+here_is_source
+
+CURBRANCH=$(git branch | grep '^\*' | cut -b3-)
+
+current_must_not_have_uncommitted ()
+{
+	DIFFLINE=$(git diff | wc -l)
+	if [ $DIFFLINE -ne 0 ]; then
+		elog "current branch has uncommitted changes"
+	fi
+}
+
+update_branch ()
+{
+	GITBRANCH="$1"
+	git checkout $GITBRANCH
+	git pull -u origin $GITBRANCH
+}
+
+back_to_current ()
+{
+	git checkout $CURBRANCH
+}
+
+if [ "$GITCMD" = "" -o "$GITCMD" = "help" ]; then
+	usage
+	exit 0
+
+elif [ "$GITCMD" = "merge-master" ]; then
+	current_must_not_have_uncommitted
+	git checkout master
+	git pull -u origin master
+	back_to_current
+	git merge master
+
+elif [ "$GITCMD" = "reset" ]; then
+	git reset --hard HEAD
+
+elif [ "$GITCMD" = "u" -o "$GITCMD" = "update" ]; then
+	current_must_not_have_uncommitted
+	update_branch master
+	back_to_current
+
+elif [ "$GITCMD" = "update-all" ]; then
+	current_must_not_have_uncommitted
+	update_branch master
+	for PGVERSION in $(echo "$SUPPORTED_VERS"); do
+		update_branch "REL${PGVERSION}_STABLE"
+	done
+
+else
+	elog "unsupported command was specified: $GITCMD"
+fi
