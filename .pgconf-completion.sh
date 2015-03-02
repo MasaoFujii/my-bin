17b92fb78a3181303a8e58609f805204bbcab336
MasaoFujii <masao.fujii@gmail.com>
2015-03-02 20:45:32 +0900

    Refactor .pgconf-complete.sh.

--- .pgconf-completion.sh
+++ .pgconf-completion.sh
@@ -2,17 +2,15 @@ _pgconf()
 {
 	. .completion-common.sh
 
-	if [ "$PREVWORD" != "-c" -a "$PREVWORD" != "-d" -a "$PREVWORD" != "-s" ]; then
-		COMPREPLY=( $(compgen -f ${CURWORD}) )
-		return 0
-	fi
-
-	WORDLIST=$(pgconf.sh --showall 2> /dev/null)
-	if [ $? -ne 0 ]; then
-		return 0
-	fi
+	case "$PREVWORD" in
+		-c|-d|-s)
+			WORDLIST=$(pgconf.sh --showall 2> /dev/null)
+			if [ $? -ne 0 ]; then
+				WORDLIST=""
+			fi;;
+	esac
 
 	mycompgen
 }
 
-complete -F _pgconf pgconf.sh
+complete -o bashdefault -o default -F _pgconf pgconf.sh
