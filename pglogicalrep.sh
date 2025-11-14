294b98e307089ba7ed28fb72aaf222e45131a25e
Fujii Masao <fujii@postgresql.org>
2025-11-14 14:08:37 +0900

    pglogicalrep.sh: Support replication slot synchronization.

--- pglogicalrep.sh
+++ pglogicalrep.sh
@@ -4,6 +4,16 @@
 
 PUBDATA=data
 SUBDATA=sub1
+SBYDATA=sby1
+
+PUBPORT=5432
+SUBPORT=5433    ## 5434 if --slotsync
+SBYPORT=5433
+
+SUBCONN="port=$PUBPORT"
+SUBOPT=
+
+SLOTSYNC=false
 
 usage ()
 {
@@ -11,7 +21,10 @@ cat <<EOF
 $PROGNAME sets up the publisher and subscriber for logical replication.
 
 Usage:
-  $PROGNAME
+  $PROGNAME [OPTIONS]
+
+Options:
+  --slotsync  uses replication slot sync
 EOF
 }
 
@@ -20,6 +33,8 @@ while [ $# -gt 0 ]; do
 		"-?"|--help)
 			usage
 			exit 0;;
+		--slotsync)
+			SLOTSYNC=true;;
 		*)
 			elog "invalid option: $1";;
 	esac
@@ -30,17 +45,28 @@ here_is_installation
 pgsql_is_dead
 validate_logical_replication
 
-pginitdb.sh $PUBDATA
-exit_on_error
-
-pgconf.sh -c log_line_prefix "'$LOGLINEPREFIX $PUBDATA '" $PUBDATA
-pgstart.sh -w $PUBDATA
+if [ "$SLOTSYNC" = "false" ]; then
+	pginitdb.sh $PUBDATA
+	exit_on_error
+	pgstart.sh -w $PUBDATA
+else
+	pgsr.sh --slot
+	pgconf.sh -c sync_replication_slots on $SBYDATA
+	pgconf.sh -c hot_standby_feedback on $SBYDATA
+	PREVCONNINFO=$(pgconf.sh -s primary_conninfo $SBYDATA)
+	pgconf.sh -c primary_conninfo "'$PREVCONNINFO dbname=postgres'" $SBYDATA
+	pgconf.sh -c synchronized_standby_slots "'$SBYDATA'" $ACTDATA
+	pgreload.sh $SBYDATA
+	pgreload.sh $ACTDATA
+	SUBPORT=5434
+	SUBCONN="host=localhost,localhost port=${PUBPORT},${SBYPORT} target_session_attrs=primary"
+	SUBOPT="WITH (failover = 'true')"
+	$PGBIN/psql -p $SBYPORT -c "SELECT pg_create_physical_replication_slot('$SBYDATA')"
+fi
 
 pginitdb.sh $SUBDATA
 exit_on_error
-
-pgconf.sh -c port 5433 $SUBDATA
-pgconf.sh -c log_line_prefix "'$LOGLINEPREFIX $SUBDATA '" $SUBDATA
+pgconf.sh -c port $SUBPORT $SUBDATA
 pgstart.sh -w $SUBDATA
 
 pgtbl.sh -n 0 $PUBDATA
@@ -51,7 +77,7 @@ CREATE PUBLICATION tpub FOR TABLE t;
 EOF
 
 pgtbl.sh -n 0 $SUBDATA
-cat <<EOF | $PGBIN/psql -p 5433
+cat <<EOF | $PGBIN/psql -p $SUBPORT
 ALTER TABLE t ADD PRIMARY KEY (i);
-CREATE SUBSCRIPTION tsub CONNECTION '' PUBLICATION tpub;
+CREATE SUBSCRIPTION tsub CONNECTION '${SUBCONN}' PUBLICATION tpub $SUBOPT;
 EOF
